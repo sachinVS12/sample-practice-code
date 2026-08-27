4f2d369efbe07cd6547f785362e439e2efeebd6e
Sachin V S <vs1sachi12@gmail.com>
2026-08-27 15:14:03 +0530

    signup code

--- node.js
+++ node.js
@@ -1,94 +1,51 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Message = require("./models/message-model");
+const User = require("./models/user-model");
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/message", async (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
-    const { email, name, age } = req.body;
-    const message = await Message.create({ email, name, age });
-    res.status(201).json({ success: true, data: message });
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    const token = await user.generateToken();
+    res.status(201).json({ success: true, token });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 });
 
-app.get("/message", async (req, res) => {
+app.post("/signin", async (req, res) => {
   try {
-    const message = await Message.find({});
-    res.status(201).json({ success: true, data: message });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
-});
-
-app.delete("/message/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const message = await Message.findByIdAndDelete(id);
-    res.status(201).json({ success: true, data: [] });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
-});
-
-app.put("/message/:id", async (req, res) => {
-  try {
-    const message = await Message.findByIdAndUpdate(req.params.id, req.body, {
-      returnDocument: "after",
-      runValidators: true,
-    });
-    if (!message) {
+    const { name, email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
       return res
-        .status(404)
-        .json({ success: false, message: `no fuond with ${req.params.id}` });
+        .status(400)
+        .json({ success: false, message: "email not registered" });
     }
-    const updatedMessage = await Message.findById(req.params.id);
-    res.status(201).json({ success: true, data: updatedMessage });
+    const validate = await user.verifypass(password);
+    if (!validate) {
+      res
+        .status(400)
+        .json({ success: false, message: "password is not valid" });
+    }
+    const token = await user.generateToken();
+    res.status(200).json({ success: true, token });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 });
-
-// app.put("/message/:id", async (req, res) => {
-//   try {
-//     const updatedMessage = await Message.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       {
-//         returnDocument: "after",
-//         runValidators: true,
-//       },
-//     );
-
-//     if (!updatedMessage) {
-//       return res.status(404).json({
-//         success: false,
-//         message: `No resource found with id ${req.params.id}`,
-//       });
-//     }
-
-//     res.status(200).json({
-//       success: true,
-//       data: updatedMessage,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
 
 mongoose
-  .connect("mongodb://localhost:27017/crud")
+  .connect("mongodb://localhost:27017/sachin")
   .then(() => {
     console.log("Database connection successful!");
     app.listen(8000, () => {
-      console.log("listening on running port number 8000");
+      console.log("Listining on running port number 8000");
     });
   })
   .catch(() => {
