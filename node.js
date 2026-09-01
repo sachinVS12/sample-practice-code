37d44fe9ae26aa7673994dc11e845fff10460a34
Sachin V S <vs1sachi12@gmail.com>
2026-09-01 16:16:03 +0530

    practice curd

--- node.js
+++ node.js
@@ -1,3 +1,74 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const Message = require("./models/message-model");
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.post("/message", async (req, res) => {
+//   try {
+//     const { email, name, age } = req.body;
+//     const message = await Message.create({ email, name, age });
+//     res.status(200).json({ success: true, data: message });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// });
+
+// app.get("/message", async (req, res) => {
+//   try {
+//     const message = await Message.find({});
+//     res.status(200).json({ success: true, data: message });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// });
+
+// app.delete("/message/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const message = await Message.findByIdAndDelete(id);
+//     res.status(200).json({ success: true, data: [] });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.mesage });
+//   }
+// });
+
+// app.put("/message/:id", async (req, res) => {
+//   try {
+//     const updateMessage = await Message.findByIdAndUpdate(
+//       req.params.id,
+//       req.body,
+//       {
+//         returnDocument: "after",
+//         runValidators: true,
+//       },
+//     );
+//     if (!updateMessage) {
+//       res
+//         .status(404)
+//         .json({ success: false, message: `no found with id ${req.params}` });
+//     }
+//     res.status(200).json({ success: true, data: updateMessage });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// });
+
+// mongoose
+//   .connect("mongodb://localhost:27017/crud")
+//   .then(() => {
+//     console.log("Database connection successful!");
+//     app.listen(8000, () => {
+//       console.log("Listing on port running number 8000");
+//     });
+//   })
+//   .catch(() => {
+//     console.log("Databse connection failed");
+//   });
+
+// server.js
 const express = require("express");
 const mongoose = require("mongoose");
 const Message = require("./models/message-model");
@@ -9,8 +80,8 @@ app.use(express.urlencoded({ extended: false }));
 app.post("/message", async (req, res) => {
   try {
     const { email, name, age } = req.body;
-    const message = await Message.create({ email, name, age });
-    res.status(200).json({ success: true, data: message });
+    const message = await Message.create({ email, age, name });
+    res.status(201).json({ success: true, data: message });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
@@ -31,7 +102,7 @@ app.delete("/message/:id", async (req, res) => {
     const message = await Message.findByIdAndDelete(id);
     res.status(200).json({ success: true, data: [] });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.mesage });
+    res.status(500).json({ success: false, message: error.message });
   }
 });
 
@@ -40,15 +111,12 @@ app.put("/message/:id", async (req, res) => {
     const updateMessage = await Message.findByIdAndUpdate(
       req.params.id,
       req.body,
-      {
-        returnDocument: "after",
-        runValidators: true,
-      },
+      { returnDocument: "after", runValidators: true },
     );
     if (!updateMessage) {
-      res
+      return res
         .status(404)
-        .json({ success: false, message: `no found with id ${req.params}` });
+        .json({ success: false, message: `no found with id ${req.params.id}` });
     }
     res.status(200).json({ success: true, data: updateMessage });
   } catch (error) {
@@ -59,11 +127,11 @@ app.put("/message/:id", async (req, res) => {
 mongoose
   .connect("mongodb://localhost:27017/crud")
   .then(() => {
-    console.log("Database connection successful!");
+    console.log("!Database coonection successful!");
     app.listen(8000, () => {
-      console.log("Listing on port running number 8000");
+      console.log("listining on port number is 8000");
     });
   })
   .catch(() => {
-    console.log("Databse connection failed");
+    console.log("Database connection failed");
   });
