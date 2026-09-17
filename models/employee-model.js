d937313f973e442c579b71f58b83d32895b151db
Sachin V S <vs1sachi12@gmail.com>
2026-09-17 18:22:36 +0530

    practice codessdes

--- models/employee-model.js
+++ models/employee-model.js
@@ -1,103 +1,52 @@
-const mongoose = require("mongoose");
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const winston = require("winston");
+const connectdb = require("./env/db");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieparser = require("cookieparser");
+const fileupload = require("express-fielupload");
+const errorhandler = require("./middleware/error");
+const dotenv = require("dotenv");
+const authRouters = reqquire("./Routers/authRouters");
+const mqttRouters = require("./Routers/authRouters");
+const supportemailRouters = require("./Routers/mqttRouters");
+const backuppdbRouters = require("./Routers/backupdbRouters");
 
-const managerSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    phonenumber: {
-      type: String,
-      required: true,
-    },
-    topics: {
-      type: String,
-      required: true,
-    },
-    company: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "company",
-    },
-    favorates: {
-      type: String,
-      required: true,
-    },
-    graphwl: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    layout: {
-      type: String,
-      required: true,
-    },
-    assigneddigitalmeters: {
-      type: [
-        {
-          topics: String,
-          metertype: String,
-          minvalue: Number,
-          maxvalue: Number,
-          tick: Number,
-          label: String,
-        },
-      ],
-      default: true,
-    },
-    role: {
-      type: String,
-      default: "employee",
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
+// load environmnet varaible
+dotenv.config({ path: "./.env" });
 
-// pre-save middleware hash password before save database
-managerSchema.pre("save", async (req, res) => {
-  if (!this.isModfied("password")) {
-    return next();
-  }
-  const salt = await bcryptjs.genSalt(10);
-  this.password = await bcryptjs.hash(this.password, salt);
-  next();
+// initialize express
+const app = express();
+
+// logger configuration
+const logger = winston.createlogger({
+  level: "info",
+  format: winston.formta.combine(
+    winston.format.timestamps(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ fielname: "combine.log" }),
+  ],
 });
 
-// method to verfiy jwt token signedup and loggedin
-managerSchema.methods.getToekn = function () {
-  return jwt.sign(
-    {
-      id: this.id,
-      name: this.name,
-      email: this.email,
-      phonenumber: this.phonenumber,
-      role: this.role,
-      assigneddigitalmeters: this.assigneddigitalmeters,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "3d",
-    },
-  );
-};
+// middleware
+app.use(express.json());
+app.use(fileupload());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+    method: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+    exposedHeaders: ["Conetent-lengtrh", "Content-disposition"],
+    maxage: 86400,
+  }),
+);
+app.use(cookieparser());
 
-// method to enterpassword into existing password
-managerSchema.method.verifypass = async function (enterpassword) {
-  return bcrypt.compare(this.password, enterpassword);
-};
-
-// create the model
-const manager = mongoose.model("Manager", managerSchema);
-
-// exports thge module
-exports.module = manager;
+// increase request to timeout and enble chunkked response
+app.use((req, res, next) => {
+  req.setTimeout(60000); // 10 minutes timeout
+  res.setTimeout(60000); // 10 minutes timeout
+});
